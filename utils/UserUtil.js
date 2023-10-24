124d7f4cfaabd08eecbb84885c1e0aa998924ab8
Bryan0831 <2202191I@student.tp.edu.sg>
2023-10-24 11:02:13 +0800

    Implemented the backend for login

--- utils/UserUtil.js
+++ utils/UserUtil.js
@@ -1,11 +1,13 @@
 const { User } = require('../models/User');
 const fs = require('fs').promises;
+
 async function readJSON(filename) {
     try {
         const data = await fs.readFile(filename, 'utf8');
         return JSON.parse(data);
     } catch (err) { console.error(err); throw err; }
 }
+
 async function writeJSON(object, filename) {
     try {
         const allObjects = await readJSON(filename);
@@ -14,6 +16,7 @@ async function writeJSON(object, filename) {
         return allObjects;
     } catch (err) { console.error(err); throw err; }
 }
+
 async function register(req, res) {
     try {
         const email = req.body.email;
@@ -29,6 +32,28 @@ async function register(req, res) {
         return res.status(500).json({ message: error.message });
     }
 }
+async function login(req, res) {
+    try {
+        const email = req.body.email;
+        const password = req.body.password;
+        const allUsers = await readJSON('utils/users.json');
+        var validCredentials = false;
+        for (var i = 0; i < allUsers.length; i++) {
+            var currUser = allUsers[i];
+            if (currUser.email == email && currUser.password == password)
+                validCredentials = true;
+        }
+        if (validCredentials) {
+            return res.status(201).json({ message: 'Login successful!' });
+        } else {
+            return res.status(500).json({ message: 'Invalid credentials!' });
+        }
+    } catch (error) {
+        return res.status(500).json({ message: error.message });
+    }
+}
+
+
 module.exports = {
-    readJSON, writeJSON, register
+    readJSON, writeJSON, register, login
 };
