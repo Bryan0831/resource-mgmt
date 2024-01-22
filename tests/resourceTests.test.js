5fd8476846cdb563fad3fe9d35a29772e5bf92cf
Bryan0831 <2202191I@student.tp.edu.sg>
2024-01-23 03:58:46 +0800

    Completed the design of API route-related test cases for resources

--- tests/resourceTests.test.js
+++ tests/resourceTests.test.js
@@ -1,7 +1,7 @@
 const { describe, it } = require('mocha');
 const { expect } = require('chai');
 const fs = require('fs').promises;
-const { addResource, viewResources } = require('../utils/ResourceUtil')
+const { addResource, viewResources, editResource, deleteResource } = require('../utils/ResourceUtil')
 
 describe('Testing resource related features', () => {
     const resourcesFilePath = 'utils/resources.json';
@@ -26,21 +26,21 @@ describe('Testing resource related features', () => {
                 owner: "john@gmail.com"
             },
         };
-       
+
         const res = {
             status: function (code) {
                 expect(code).to.equal(201);
                 return this;
             },
-           
+
             json: function (data) {
                 expect(data).to.have.lengthOf(orgContent.length + 1);
                 expect(data[orgContent.length].name).to.equal(req.body.name);
             },
         };
-        
+
         await addResource(req, res);
-      
+
     });
 
 
@@ -80,6 +80,83 @@ describe('Testing resource related features', () => {
         await viewResources(req, res);
     });
 
+    it('Should edit a resource successfully', async () => {
+        const req = {
+            body: {
+                name: "Test",
+                location: "Temasek",
+                description: "Testing purpose",
+            },
+            params: {
+                id: orgContent[0].id
+            }
+        };
+        const res = {
+            status: function (code) {
+                expect(code).to.equal(201);
+                return this;
+            },
+            json: function (data) {
+                expect(data.message).to.equal('Resource modified successfully!');
+            },
+        };
+        await editResource(req, res);
+    });
 
-
+    it('Should not be able to edit resource due to invalid id', async () => {
+        const req = {
+            body: {
+                name: "Test",
+                location: "Temasek",
+                description: "Testing purpose",
+            },
+            params: {
+                id: "ABCDEFG"
+            }
+        };
+        const res = {
+            status: function (code) {
+                expect(code).to.equal(500);
+                return this;
+            },
+            json: function (data) {
+                expect(data.message).to.equal('Error occurred, unable to modify!');
+            },
+        };
+        await editResource(req, res);
+    });
+    it('Should delete a resource successfully', async () => {
+        const req = {
+            params: {
+                id: orgContent[0].id
+            }
+        };
+        const res = {
+            status: function (code) {
+                expect(code).to.equal(201);
+                return this;
+            },
+            json: function (data) {
+                expect(data.message).to.equal('Resource deleted successfully!');
+            },
+        };
+        await deleteResource(req, res);
+    });
+    it('Should not be able to delete resource due to invalid id', async () => {
+        const req = {
+            params: {
+                id: "ABCDEFG"
+            }
+        };
+        const res = {
+            status: function (code) {
+                expect(code).to.equal(500);
+                return this;
+            },
+            json: function (data) {
+                expect(data.message).to.equal('Error occurred, unable to delete!');
+            },
+        };
+        await deleteResource(req, res);
+    });
 });
